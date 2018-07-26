df1628895de11fd9692788890089ebeae5f1b436
Sushi Chanrai <sushi@pathworks.co>
2018-07-26 00:18:12 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -23,5 +23,5 @@ class Song
     self.artist = artiste #assign the song(self -> song instance) to the artist
     artiste.add_song(self) #artiste is the artist instance, collaborate with the `Artist` class by calling on the `Artist#add_song(some_song)` method.
   end
-
+_FILE_
 end
